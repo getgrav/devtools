a93fab86236508bec3b3c9cb097f9a7417eb71b6
Djamil Legato <djamil@djamil.it>
2015-06-16 23:47:25 -0700

    ensuring the data is json

--- gh-assets.js
+++ gh-assets.js
@@ -3,8 +3,15 @@ var args = process.argv.slice(2);
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(data) {
+    if (data.length <= 2 || !data.match(/^{.+}$/)) {
+        process.stdout.write('');
+
+        return;
+    }
+
     var response = JSON.parse(data),
         assets = response.assets;
+
     if (!assets) { process.stdout.write(''); }
     else {
         for (var k in assets) {
